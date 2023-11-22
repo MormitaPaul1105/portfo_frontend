d2a20bc904c9dbb1bbce27b807770039f0799fd7
Marmita Paul <96205953+MormitaPaul1105@users.noreply.github.com>
2023-11-23 04:36:42 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: 'https://mormitapaul1105.github.io/dashboard/about',
-  reactStrictMode: true,
-  basePath: 'https://mormitapaul1105.github.io/dashboard/certificate',
-  reactStrictMode: true,
-  basePath: 'https://mormitapaul1105.github.io/dashboard/projects', 
-  reactStrictMode: true,
-  basePath: 'https://mormitapaul1105.github.io/dashboard/skill', 
-  reactStrictMode: true,
-  basePath: 'https://mormitapaul1105.github.io/dashboard/education', 
-  reactStrictMode: true,
-  basePath: 'https://mormitapaul1105.github.io/dashboard/experience', 
+ 
   reactStrictMode: true,
 };
 
